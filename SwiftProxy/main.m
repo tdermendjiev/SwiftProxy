e522d055dd756a17b846cb54d8399a79b8007b0d
Teodor Dermendjiev <tdermendjievft@gmail.com>
2022-11-23 11:29:30 +0200

    Initial Commit

---
+++ SwiftProxy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SwiftProxy
+//
+//  Created by Teodor Dermendzhiev on 23.11.22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
