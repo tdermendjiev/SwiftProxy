101ffa62beecb49c554960d2fa6363e0d60e9be6
Teodor Dermendjiev <tdermendjievft@gmail.com>
2022-11-29 14:09:10 +0200

    first commit

--- SwiftProxy/SwiftClass.swift
+++ SwiftProxy/SwiftClass.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+
+class AClass{
+    
+    let count: Int
+    
+    init(count: Int) {
+        self.count = count
+    }
+    
+    func printCount() {
+        print(count)
+    }
+    
+}
+
+
+@_cdecl("aclass_create")
+public func AClass_create(count: Int) -> OpaquePointer {
+    let type = AClass(count: count)
+    let retained = Unmanaged.passRetained(type).toOpaque()
+//    retained.deallocate()
+//    print(retained)
+    return OpaquePointer(retained)
+}
+
+@_cdecl("aclass_printCount")
+public func AClass_printCount(inst: UnsafeRawPointer) {
+    let i = Unmanaged<AClass>.fromOpaque(inst).takeUnretainedValue()
+    i.printCount()
+}
+
+@_cdecl("test_getProp_int")
+func getIntProp(inst: UnsafeRawPointer, key: UnsafeMutablePointer<Int8>) -> Int32 {
+    let str = String(cString: key)
+    let obj = Unmanaged<AClass>.fromOpaque(inst).takeUnretainedValue()
+    let mirrorLib = Mirror(reflecting: obj);
+    if let propValue = mirrorLib.children.first(where: { $0.label == str })?.value as? Int {
+        return Int32(propValue)
+    }
+    return 0
+}
--- SwiftProxy/main.m
+++ SwiftProxy/main.m
@@ -7,10 +7,19 @@
 
 #import <Foundation/Foundation.h>
 
+void* aclass_create(int);
+void* aclass_printCount(void*);
+int test_getProp_int(void*, void*);
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
     }
+    
+    void* inst = aclass_create(5);
+    aclass_printCount(inst);
+    NSNumber* number = [[NSNumber alloc] initWithInt:test_getProp_int(inst, "count")];
+    NSLog(@"%@", number);
     return 0;
 }
